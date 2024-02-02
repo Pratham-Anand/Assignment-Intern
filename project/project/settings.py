60c560e749e25e752e585910b0cc3d7bfcbcd054
Pratham-Anand <anandpratham13@gmail.com>
2024-02-02 11:51:03 +0530

    added footer and .env
    added footer and env in gitignore

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+from dotenv import load_dotenv  # type: ignore
+
+# Load environment variables from .env
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+9k+k7j+!syu*%+p&a&0uz_$s-8-h0r(dervnu%@a2_-0^g8su'
+SECRET_KEY= os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}"
